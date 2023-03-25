6ed5fb6b20894904d77c25bf1951ef7b67f2ae66
William Neumann <williamrn1996@gmail.com>
2023-03-25 13:22:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  let [lista,setLista] = useState([])
+  let [novoItem, setNovoItem] = useState("")
+
+  useEffect(()=> {
+    setLista(["Item 1", "Item 2", "Item 3"])
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div className='new-item'>
+        <input placeholder='Tarefa' value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text"></input>
+        <button onClick={()=> adicionarNovoItem()}>Adicionar</button>
+      </div>
+      <ul className='todo-list'>
+        {lista.map((item,index)=> (
+          <li key={index} className="todo-item">
+            {item}
+            <button onClick={()=>deletarItem(index)}>Deletar</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+    );
+    function adicionarNovoItem(){
+      if (novoItem.length <= 0){
+        alert("Insira um item")
+        return
+      }
+      let itemIndex = lista.indexOf(novoItem)
+      if (itemIndex >=0) {
+        alert("Tarefa ja existe!")
+        return
+      }
+      setLista([...lista, novoItem])
+      setNovoItem("")
+    }
+
+    function deletarItem(index){
+      let tempArray = [...lista]
+      tempArray.splice(index,1)
+      setLista(tempArray)
+    }
 }
 
 export default App;
